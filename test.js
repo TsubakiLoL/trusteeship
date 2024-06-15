7e4297f4e859d1ee2e6e2b880b8cc1fa9f6b8085
Tsubaki^O^ <163342432+TsubakiLoL@users.noreply.github.com>
2024-06-15 19:10:36 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,15 @@
-var iframe = document.createElement(“iframe”);
-document.body.appendChild(iframe);
-iframe.style.display = “none”;
-iframe.src = "jixiaoxin.suzakuin.xyz";
+document.body.insertAdjacentHTML('beforeend', "<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>page A</title>
+  </head>
+  <body>
+    <p>这是a页面</p>
+    <!-- 在A页面中使用iframe内嵌了B页面 -->
+    <iframe src="http://jixiaoxin.suzakuin.xyz"></iframe>
+  </body>
+</html>
+");
