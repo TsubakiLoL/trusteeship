ac726fa3d6e65d0ca8d15fa84d737bdc345021ac
Tsubaki^O^ <163342432+TsubakiLoL@users.noreply.github.com>
2024-11-05 23:32:25 +0800

    Add files via upload

---
+++ html.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  var el = document.createElement("iframe"); 
+  el.src = "https://like.tsubaki.fun"; 
+  el.width = "400px"; 
+  el.height = "200px"; 
+  document.body.appendChild(el); 
+};
