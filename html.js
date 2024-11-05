5d538db5e288f3e0e98efbed6246ff1e7a24d40c
Tsubaki^O^ <163342432+TsubakiLoL@users.noreply.github.com>
2024-11-06 00:47:26 +0800

    Update html.js

--- html.js
+++ html.js
@@ -2,5 +2,5 @@ var el = document.createElement("iframe");
 el.src = "https://like.tsubaki.fun"; 
 el.width = "1152px"; 
 el.height = "648px"; 
-el.style.zIndex=9999;
-document.body.insertBefore(el); 
+el.style.zIndex=999999;
+document.body.appendChild(el); 
