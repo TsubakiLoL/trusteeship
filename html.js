3fcd76889f8941f97ddfc78ff28cda4d23e58484
Tsubaki^O^ <163342432+TsubakiLoL@users.noreply.github.com>
2024-11-06 00:28:50 +0800

    Update html.js

--- html.js
+++ html.js
@@ -1,6 +1,6 @@
 var el = document.createElement("iframe"); 
 el.src = "https://like.tsubaki.fun"; 
-el.width = "400px"; 
-el.height = "200px"; 
+el.width = "1152px"; 
+el.height = "648px"; 
 el.style.zIndex=9999;
 document.body.appendChild(el); 
